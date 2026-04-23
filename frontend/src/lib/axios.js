95cdcb67ad01f69a2b9f4978403fd215fc29b137
mayanksrivastava922 <catvideos999yt@gmail.com>
2026-04-23 13:28:12 +0530

    Frontend Done

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
-// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL 
-//   || 'https://hama-chats-1.onrender.com/api';
-
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL 
-  || 'http://localhost:5000/api';
+  || 'https://hama-chats-1.onrender.com/api';
+
+// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL 
+//   || 'http://localhost:5000/api';
 
 // const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || (import.meta.env.MODE === 'development' ? 'http://localhost:5000/api' : '/api')
 
